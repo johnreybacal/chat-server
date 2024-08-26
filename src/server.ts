04414f0478ca171d94b4abf1a7621ff47a013baa
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-08-26 20:14:14 +0800

    add logs

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,20 @@ import { Server } from "socket.io";
 export const server = new Server({ cors: { origin: "http://localhost:3000" } });
 
 server.on("connection", (socket) => {
+  console.log(`connection: ${socket.id} ${socket.handshake.auth.username}`);
+
   socket.on("joinRoom", (room: string) => {
+    console.log(
+      `joinRoom: ${socket.id} ${socket.handshake.auth.username} ${room}`
+    );
+
     socket.join(room);
   });
   socket.on("sendMessage", (room: string, message: string) => {
+    console.log(
+      `sendMessage: ${socket.id} ${socket.handshake.auth.username} ${room}`
+    );
+
     server.to(room).emit("distributeMessage", room, {
       message,
       socketId: socket.id,
