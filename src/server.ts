5827f60ed574aeff22ba81763710ca53b98026d7
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-08-27 17:07:42 +0800

    add connectionStateRecovery and lessen log verbosity

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,19 @@
 import { Server } from "socket.io";
-export const server = new Server({ cors: { origin: "http://localhost:3000" } });
+export const server = new Server({
+  cors: { origin: "http://localhost:3000" },
+  connectionStateRecovery: {},
+});
 
 server.on("connection", (socket) => {
   console.log(`connection: ${socket.id} ${socket.handshake.auth.username}`);
 
   socket.on("joinRoom", (room: string) => {
-    console.log(
-      `joinRoom: ${socket.id} ${socket.handshake.auth.username} ${room}`
-    );
+    console.log(`joinRoom: ${socket.id} ${room}`);
 
     socket.join(room);
   });
   socket.on("sendMessage", (room: string, message: string) => {
-    console.log(
-      `sendMessage: ${socket.id} ${socket.handshake.auth.username} ${room}`
-    );
+    console.log(`sendMessage: ${socket.id} ${room}`);
 
     server.to(room).emit("distributeMessage", room, {
       message,
