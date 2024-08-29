2dfe2f722bbad1cf00db795ebe76eaca72b32773
Johnrey Bacal <johnrey.bacal@yahoo.com>
2024-08-29 20:33:59 +0800

    add server time

--- src/server.ts
+++ src/server.ts
@@ -6,41 +6,57 @@ export const server = new Server({
   connectionStateRecovery: {},
 });
 
+async function getTime(): Promise<String> {
+  try {
+    const response = await fetch(
+      `http://worldtimeapi.org/api/timezone/${
+        Intl.DateTimeFormat().resolvedOptions().timeZone
+      }`
+    );
+    const json = await response.json();
+    return json.utc_datetime;
+  } catch (e) {
+    return new Date().toISOString();
+  }
+}
+
 server.on("connection", (socket) => {
   console.log(`connection: ${socket.id} ${socket.handshake.auth.username}`);
 
-  const roomEvent = () => ({
+  const clientInformation = {
     socketId: socket.id,
     username: socket.handshake.auth.username,
-    date: new Date(),
-  });
+  };
 
-  socket.on("joinRoom", (room: string) => {
+  socket.on("joinRoom", async (room: string) => {
     console.log(`joinRoom: ${socket.id} ${room}`);
 
     socket.join(room);
     server.to(room).emit("roomEvent", room, {
       type: "userJoined",
-      ...roomEvent(),
+      date: await getTime(),
+      ...clientInformation,
     });
   });
-  socket.on("sendMessage", (room: string, message: string) => {
+  socket.on("sendMessage", async (room: string, message: string) => {
     console.log(`sendMessage: ${socket.id} ${room}`);
 
     server.to(room).emit("roomEvent", room, {
       type: "message",
+      date: await getTime(),
       message,
-      ...roomEvent(),
+      ...clientInformation,
     });
   });
-  socket.on("disconnecting", (reason) => {
+  socket.on("disconnecting", async (reason) => {
     console.log("disconnect", socket.id, reason);
-
+    const date = await getTime();
     for (const room of Array.from(socket.rooms)) {
       if (room !== socket.id) {
         server.to(room).emit("roomEvent", room, {
           type: "userLeft",
-          ...roomEvent(),
+          date,
+          ...clientInformation,
         });
       }
     }
